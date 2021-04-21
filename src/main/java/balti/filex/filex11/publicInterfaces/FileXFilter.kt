27122289faf2265d7fd438d5e628daed46f8dcd4
SayantanRC <sayantan.kgp@gmail.com>
2021-04-21 19:18:28 +0530

    FileXFilter.kt, FileXNameFilter.kt: Add documentation.

--- src/main/java/balti/filex/filex11/publicInterfaces/FileXFilter.kt
+++ src/main/java/balti/filex/filex11/publicInterfaces/FileXFilter.kt
@@ -1,7 +1,24 @@
 package balti.filex.filex11.publicInterfaces
 
 import balti.filex.FileX
+import balti.filex.filex11.FileX11
+import balti.filex.filexTraditional.FileXT
 
+/**
+ * This interface is made to replicate logic of [java.io.FileFilter].
+ * It is used to provide logic to filter files in all FileX listing methods. This logic is provided in the [accept] method.
+ *
+ * This interface provides a unified way to deal with both [FileXT] and [FileX11] types.
+ */
 interface FileXFilter {
+
+    /**
+     * Function to be overridden to define the logic to filter files in file lists.
+     *
+     * @param file The FileX object, pointinig to a file / directory to be evaluated with the filtering logic inside the method.
+     * An alternate interface [FileXNameFilter] is also present, which focuses on the string name of the files, rather than FileX objects.
+     *
+     * @return `true` if the file qualifies to be included in the filtered list of files, `false` otherwise.
+     */
     fun accept(file: FileX): Boolean
 }
--- src/main/java/balti/filex/filex11/publicInterfaces/FileXNameFilter.kt
+++ src/main/java/balti/filex/filex11/publicInterfaces/FileXNameFilter.kt
@@ -1,7 +1,25 @@
 package balti.filex.filex11.publicInterfaces
 
 import balti.filex.FileX
+import balti.filex.filexTraditional.FileXT
+import balti.filex.filex11.FileX11
 
+/**
+ * This interface is made to replicate logic of [java.io.FilenameFilter].
+ * It is used to provide logic to filter files in all FileX listing methods. This logic is provided in the [accept] method.
+ *
+ * This interface provides a unified way to deal with both [FileXT] and [FileX11] types.
+ */
 interface FileXNameFilter {
+
+    /**
+     * Function to be overridden to define the logic to filter files in file lists.
+     *
+     * @param dir Directory in which the file to be evaluated for the logic was found in.
+     * @param name Name of the file to be evaluated with the filtering logic.
+     * If actual FileX object is required for the logic, please check out [FileXFilter], rather than creating a FileX instance for every new file.
+     *
+     * @return `true` if the file qualifies to be included in the filtered list of files, `false` otherwise.
+     */
     fun accept(dir: FileX, name: String): Boolean
 }
