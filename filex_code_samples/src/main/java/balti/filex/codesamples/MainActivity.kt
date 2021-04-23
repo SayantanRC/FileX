a7197dd869d0d81a0e5680861196506b15529264
SayantanRC <sayantan.kgp@gmail.com>
2021-04-23 11:38:17 +0530

    New module - filex_code_samples. This module can be run as an app. It already is configured to use the FileX library.

---
+++ filex_code_samples/src/main/java/balti/filex/codesamples/MainActivity.kt
@@ -0,0 +1,15 @@
+package balti.filex.codesamples
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import balti.filex.FileXInit
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Initialize FileX library
+        FileXInit(this, false)
+    }
+}
