27b1334880daa2444c9ec3842c5af97405b8a16e
SayantanRC <sayantan.kgp@gmail.com>
2021-04-27 20:09:16 +0530

    [CODE SAMPLE] - Demo for requesting permission.

--- filex_code_samples_app/app/src/main/java/balti/filex/codesamples/MainActivity.kt
+++ filex_code_samples_app/app/src/main/java/balti/filex/codesamples/MainActivity.kt
@@ -1,15 +1,53 @@
 package balti.filex.codesamples
 
+import android.annotation.SuppressLint
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import balti.filex.FileX
 import balti.filex.FileXInit
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         // Initialize FileX
         FileXInit(this, false)
+
+        request_permission.setOnClickListener {
+
+            // Get root location if permission granted.
+            fun getRootLocation(): String {
+                val root = FileX.new("/")
+                return root.canonicalPath
+            }
+
+            // Check FileX permission
+            if (!FileXInit.isUserPermissionGranted()) {
+
+                // Not granted. Request for access.
+                FileXInit.requestUserPermission { resultCode, data ->
+                    // for permission granted
+                    if (resultCode == Activity.RESULT_OK) {
+                        request_result.text = "${getString(R.string.granted)}.\n"
+                        request_result.append("Root location: ${getRootLocation()}")
+                    }
+                    else {
+                        // On system picker, if the user keeps pressing back and exits out of it, then this block works.
+                        request_result.text = "${getString(R.string.access_denied)}\n"
+                    }
+                }
+            }
+            else {
+
+                // Already granted
+                request_result.text = "${getString(R.string.already_granted)}\n"
+                request_result.append("Root location: ${getRootLocation()}")
+            }
+        }
     }
 }
